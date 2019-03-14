cff97a3e9dee0b0df74bcbe7e6c78ebbe771ec03
Bård Røtzer <bard@kartoteket.as>
2019-03-14 15:08:22 +0200

    Update the who part of the app

--- src/Who.svelte
+++ src/Who.svelte
@@ -1,14 +1,20 @@
-	<div class="sm:flex sm:items-center px-6 py-4">
+	<div class="sm:flex sm:items-center px-6 pt-4 pb-20">
     <img class="block h-48 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="img/jacob.jpg" alt="">
     <div class="text-center sm:text-left sm:flex-grow">
       <div class="mb-4">
         <p class="text-xl leading-tight">Håkon Jacob Røthing</p>
-        <p class="text-sm leading-tight text-grey-dark">Håkon Jakob Røthing is an adventurer and manager based in Oslo with his wife and two sons. He has previously biked from North Cape to Cape Town and walked along the Blue Nile. He believes in dreams, and the value of making them come true.</p>
+        <p class="leading-tight text-grey-dark py-4">Håkon Jakob Røthing is an adventurer and manager based in Oslo with his wife and two sons. He has previously biked from North Cape to Cape Town and walked along the Blue Nile. He believes in dreams, and the value of making them come true.</p>
+        <p class="leading-tight text-grey-dark py-4">Connect with Jakob on facebbok to follow the trip as it progresses</p>
+      </div>
+      <div>
+        <button class=" font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Connect with me on facebook</button>
       </div>
     </div>
   </div>
-
-
+  <div class="hidden sm:hidden  lg:block xl:block md:block">
+    <img alt="row" src="img/people.jpg" class="frontpage__image"/>
+    <p class="leading-tight text-grey-dark py-2 text-sm">(from left) Niklas, Jakob, Ralph (Skipper) and James </p>
+  </div>
   <script>
 
   </script>
