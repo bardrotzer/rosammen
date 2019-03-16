5d72c41ea9424ff896e2ae6340b06d49d4e28242
Bård Røtzer <bard@kartoteket.as>
2019-03-16 13:09:10 +0200

    Add and fix the link of donations page

--- src/Receiver.svelte
+++ src/Receiver.svelte
@@ -1,16 +1,16 @@
 <div class="flex-1  max-w-sm mx-auto">
   <div class=" bg-white m-3 border-grey-lighter rounded overflow-hidden shadow-lg text-center">
   <!-- <div class="flex-1 max-w-sm m-3 border border-grey-lighter rounded overflow-hidden bg-white shadow-lg text-center"> -->
-    <img class="w-1/3" src={logo} alt="{name}">
-    <div class="p-3">
+    <img class="w-1/5" src={logo} alt="{name}">
+    <div class="px-3 py-1">
       <div class="font-bold text-xl mb-2">{name}</div>
       <p class="text-grey-darker text-sm">{description}</p>
-      <p class="text-grey-darker text-sm"><a href="{link}">{name}</a></p>
+      <p class="text-grey-darker text-sm"><a target="_blank" href="{link}">{name}</a></p>
       <p class="text-water-darker text-sm">{vipps}</p>
     </div>
-    <!--div class="px-6 py-4 object-bottom">
+    <!-- <div class="px-6 py-4 object-bottom">
       <button on:click="selectDonationReceiver(id)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Make a donation</button>
-    </div-->
+    </div> -->
   </div>
 </div>
 
@@ -22,6 +22,7 @@ export default {
     id: ({data}) => data.id || null,
     logo: ({data}) => 'img/' + data.logo,
     vipps: ({data}) => data.vipps,
+    link: ({data}) => data.link,
   },
   methods: {
     selectDonationReceiver(receiver) {
