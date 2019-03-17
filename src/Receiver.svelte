e7dc64fef1279d049319c6aa2c61591e98c5ea1b
Bård Røtzer <bard@kartoteket.as>
2019-03-17 23:49:27 +0200

    simplify form for receivers

--- src/Receiver.svelte
+++ src/Receiver.svelte
@@ -1,16 +1,15 @@
 <div class="flex-1  max-w-sm mx-auto">
-  <div class=" bg-white m-3 border-grey-lighter rounded overflow-hidden shadow-lg text-center">
+  <div class=" bg-white my-1 mx-3 border-grey-lighter rounded overflow-hidden shadow-lg text-center">
   <!-- <div class="flex-1 max-w-sm m-3 border border-grey-lighter rounded overflow-hidden bg-white shadow-lg text-center"> -->
     <img class="w-1/5" src={logo} alt="{name}">
     <div class="px-3 py-1">
-      <div class="font-bold text-xl mb-2">{name}</div>
+      <div class="font-bold text-l mb-2"><a target="_blank" href="{link}">{name}</a></div>
       <p class="text-grey-darker text-sm">{description}</p>
-      <p class="text-grey-darker text-sm"><a target="_blank" href="{link}">{name}</a></p>
-      <p class="text-water-darker text-sm">{vipps}</p>
+      <!-- <p class="text-water-darker text-sm">{vipps}</p> -->
     </div>
-    <!-- <div class="px-6 py-4 object-bottom">
+    <div class="px-6 py-3 object-bottom">
       <button on:click="selectDonationReceiver(id)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Make a donation</button>
-    </div> -->
+    </div>
   </div>
 </div>
 
