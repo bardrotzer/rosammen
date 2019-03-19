9da5c8acb01d0a691a4ba14e728611aadcb34672
Bård Røtzer <bard@kartoteket.as>
2019-03-19 16:18:53 +0200

    make the donation screen responsive

--- src/Receiver.svelte
+++ src/Receiver.svelte
@@ -1,17 +1,20 @@
-<div class="flex-1  max-w-sm mx-auto">
-  <div class=" bg-white my-1 mx-3 border-grey-lighter rounded overflow-hidden shadow-lg text-center">
-  <!-- <div class="flex-1 max-w-sm m-3 border border-grey-lighter rounded overflow-hidden bg-white shadow-lg text-center"> -->
-    <img class="w-1/5" src={logo} alt="{name}">
-    <div class="px-3 py-1">
-      <div class="font-bold text-l mb-2"><a target="_blank" href="{link}">{name}</a></div>
-      <p class="text-grey-darker text-sm">{description}</p>
-      <!-- <p class="text-water-darker text-sm">{vipps}</p> -->
-    </div>
-    <div class="px-6 py-3 object-bottom">
-      <button on:click="selectDonationReceiver(id)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Make a donation</button>
+<div class="max-w-sm xl:max-w-md mx-auto font-sans font-light">
+  <div class="bg-c-lightgrey my-1 mx-3 border-grey-lighter rounded overflow-hidden shadow-lg">
+    <div>
+      <img class="h-24 lg:h-32 xl:h-48 float-left rounded-full border-2 m-4 border-c-orange" src={logo} alt="{name}">
+      <a class="text-c-orange pt-6 pb-2 xl:pb-6 block md:text-xl lg:text-2xl xl:text-4xl" target="_blank" href="{link}">{name}</a>
+        <p class="text-white text-sm pb-4 xl:pb-8 md:text-base lg:text-lg xl:text-xl">{description}</p>
+        <div class="w-full px-6 py-3 md:py-6 xl:py-6 text-center">
+          <button on:click="selectDonationReceiver(id)" class="text-xs xl:text-lg lg:text-base font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-c-orange text-c-orange hover:bg-c-orange hover:text-white">Make a donation</button>
+        </div>
     </div>
   </div>
 </div>
+
+
+
+
+
 
 <script>
 export default {
