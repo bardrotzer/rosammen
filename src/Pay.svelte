98d387a23533c0a891204edc6b9e5956c79014e0
Bård Røtzer <bard@kartoteket.as>
2019-03-19 16:53:41 +0200

    make initial payment screen responsive

--- src/Pay.svelte
+++ src/Pay.svelte
@@ -1,8 +1,11 @@
 
-<div class="max-w-sm m-3 border border-grey-lighter rounded overflow-hidden bg-white shadow-lg">
+<div class="max-w-sm  container mx-auto ">
   <div class=" relative">
   <div class="text-center">
-    <img class="w-2/5" src="{logo}" alt="{name}"/>
+    <p class="font-sans text-c-orange text-xl sm:text-xl md:text-4xl lg:text-4xl xl:text-4xl font-thin pt-6">
+    Donate to {name}
+    </p>
+    <img class="h-32 xl:h-48 rounded-full border-2 m-4 border-c-orange" src="{logo}" alt="{name}"/>
   </div>
 {#if paymentMethod === 'card'}
   <PaymentForm
@@ -12,42 +15,42 @@
 
 
 {:else}
-  <div class="px-6">
-    <div class="flex">
+  <div class="px-6 font-sans font-thin">
+    <div class="flex text-c-orange">
       <div class="w-1/2 h-12 content-start">
-        <label for="card-number" class="block text-grey-darker text-sm px-2">Amount (in NOK):</label>
-        <span class="text-xs px-2 text-grey">min 50</span>
+        <label for="card-number" class="block text-sm md:text-base lg:text-base xl:text-base px-2">Amount (in NOK):</label>
+        <span class="text-xs md:text-sm lg:text-sm xl:text-sm px-2 text-c-orange">min 50</span>
       </div>
-      <div class="w-1/2 h-12 content-start px-2">
-        <input type="number" bind:value=amount class="appearance-none border-grey border-b border-b-2 w-full py-2 px-2 text-grey-darker focus:outline-none focus:shadow-outline">
-      </div>
-    </div>
-    <div class="flex mb-4">
-      <div class="w-1/2 h-12 px-1">
-        <button on:click="setAmount(6000)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-grey text-grey hover:bg-grey hover:text-white">+6000 NOK</button>
-      </div>
-      <div class="w-1/2 h-12 px-1">
-        <button on:click="setAmount(300)" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-grey text-grey hover:bg-grey hover:text-white">+300 NOK</button>
+      <div class="w-1/2 px-2">
+        <input type="number" bind:value=amount class="appearance-none text-4xl text-c-orange bg-c-darkgrey border-c-orange border-b border-b-2 w-full py-2 px-2  focus:outline-none focus:shadow-outline">
       </div>
     </div>
     {#if paymentMethod === 'vipps'}
-    <div class="text-water text-xl text-center pb-6">
-      {vipps}
+    <div class="text-c-orange text-2xl text-center py-12">
+      Vipps to {name}
+      <p class="block font-bold text-4xl">{vipps}</p>
     </div>
     {:else}
-    <div class="text-grey-darker text-sm text-left pb-6">
-     Those who donate will have your name, or the name of your company (please send the logo), on the boat and do the crossing with us!
-      I suggest a donation of 5 øre per kilometre for private individuals (300 NOK), or 1 kroner per kilometre for companies (6000 NOK), but any donation is welcome.
+    <div class="text-white text-sm md:text-base lg:text-base xl:text-base text-left py-6">
+    <p>Those who donate will have your name, or the name of your company (please send the logo), on the boat and do the crossing with us!</p>
+    <p class="pt-2">I suggest a donation of 5 øre per kilometre for private individuals (<span class="no-underline text-c-orange" on:click="setAmount(300)">300</span> NOK), or 1 kroner per kilometre for companies (<span class="no-underline text-c-orange" on:click="setAmount(6000)">6000</span> NOK), but any donation is welcome.</p>
+
     </div>
     {/if}
+    {#if amount > 49}
     <div class="flex mb-4">
-      <div class="flex-1 px-5">
-        <button on:click="setPaymentMethod('card')" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white" >Pay using card</button>
+      <div class="flex-1 px-2">
+        <button on:click="setPaymentMethod('card')" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-c-blue text-blue hover:bg-blue hover:text-white" >Pay using card</button>
       </div>
-      <div class="flex-1 px-5">
-        <button on:click="setPaymentMethod('vipps')" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-orange text-orange hover:bg-orange hover:text-white">Pay using VIPPS</button>
+      <div class="flex-1 px-2">
+        <button on:click="setPaymentMethod('vipps')" class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-c-orange text-c-orange hover:bg-c-orange hover:text-white">Pay using VIPPS</button>
       </div>
     </div>
+    {:else}
+    <p class="text-c-orange text-xl md:text-2xl lg:text-2xl xl:text-2xl text-center">
+      Enter the amount to donate to continue
+    </p>
+    {/if}
   </div>
 {/if}
   </div>
