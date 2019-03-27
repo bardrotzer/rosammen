a64328fdb00ed12bfa69e39826220375d6860393
Bård Røtzer <bard@kartoteket.as>
2019-03-27 11:51:56 +0200

    Add area code data to config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,35 @@
 const conf = {
   ipStackKey:  process.env.IP_STACK_API_KEY,
+  area_codes: {
+    NO: {
+      code: '+47',
+      format: 'XXX-XX-XXX'
+    },
+    SE: {
+      code: '+46',
+      format: 'XXX-XX-XXX'
+    },
+    DK: {
+      code: '+45',
+      format: 'XXX-XX-XXX'
+    },
+    UK: {
+      code: '+44',
+      format: 'XXX-XX-XXX'
+    },
+    FR: {
+      code: '+33',
+      format: 'XXX-XX-XXX'
+    },
+    CH: {
+      code: '+41',
+      format: 'XXX-XX-XXX'
+    },
+    ES: {
+      code: '+34',
+      format: 'XXX-XX-XXX'
+    }
+  }
 };
 
 const get = (key) => {
