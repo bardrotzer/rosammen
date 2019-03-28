86b42d07333c1b46130f55d1de13440851a8d421
Bård Røtzer <bard@kartoteket.as>
2019-03-28 09:20:54 +0200

    enable vipps for non-vipps users

--- src/components/Donate.svelte
+++ src/components/Donate.svelte
@@ -1,25 +1,34 @@
 {#if !selectedReceiver}
-<div class="px-4 pt-4 pb-4 xl:pb-10 container mx-auto lg:container ">
-    <div class="font-sans text-c-orange text-4xl font-thin py-2 text-right sm:text-right md:text-left lg:text-left xl:text-left">
-			Join me on the trip
-		</div>
-    <p class="py-2 text-sm sm:text-sm md:text-xl lg:text-xl xl:text-2xl text-white font-sans  md:font-thin lg:font-thin xl:font-thin">
-      I pay for the entire trip myself and would like to contribute to causes that are near to me. Accompany me on the ocean crossing by making a donation!
-      </p>
-    <p class="pt-2 text-sm sm:text-sm md:text-xl lg:text-xl xl:text-2xl text-white font-sans  md:font-thin lg:font-thin xl:font-thin">
-    Those who donate will have your name, or the name of your company (please send the logo), on the website and do the crossing with us! I suggest a donation of {perKilometrePrivate} for private individuals, or {perKilometreBusiness} for companies, but any donation is welcome.</p>
-</div>
+  <div class="px-4 pt-4 pb-4 xl:pb-10 container mx-auto lg:container ">
+      <div class="font-sans text-c-orange text-4xl font-thin py-2 text-right sm:text-right md:text-left lg:text-left xl:text-left">
+        Join me on the trip
+      </div>
+      <p class="py-2 text-sm sm:text-sm md:text-xl lg:text-xl xl:text-2xl text-white font-sans  md:font-thin lg:font-thin xl:font-thin">
+        I pay for the entire trip myself and would like to contribute to causes that are near to me. Accompany me on the ocean crossing by making a donation!
+        </p>
+      <p class="pt-2 text-sm sm:text-sm md:text-xl lg:text-xl xl:text-2xl text-white font-sans  md:font-thin lg:font-thin xl:font-thin">
+      Those who donate will have your name, or the name of your company (please send the logo), on the website and do the crossing with us! I suggest a donation of {perKilometrePrivate} for private individuals, or {perKilometreBusiness} for companies, but any donation is welcome.</p>
+  </div>
 
-<div class="container mx-auto pt-0 md:pt-6 lg:pt-8 xl:pt-10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row items-center">
   {#if vippsEnabled || useVipps}
-    {#each receivers as receiver}
-      <Receiver data={receiver} on:selected="selectDonationReceiver(receiver.id)" />
-    {/each}
+    <div class="container mx-auto pt-0 md:pt-6 lg:pt-8 xl:pt-10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row items-center">
+      {#each receivers as receiver}
+        <Receiver data={receiver} on:selected="selectDonationReceiver(receiver.id)" />
+      {/each}
+    </div>
   {/if}
-</div>
-<div class="container mx-auto pt-0 md:pt-6 lg:pt-8 xl:pt-10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row items-center">
-  <Receiver data={receiver_rowtogether} on:selected="selectDonationReceiver(receiver_rowtogether.id)" />
-</div>
+  <div class="container mx-auto pt-0 md:pt-6 lg:pt-8 xl:pt-10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row items-center">
+    <Receiver data={receiver_rowtogether} on:selected="selectDonationReceiver(receiver_rowtogether.id)" />
+  </div>
+  {#if !vippsEnabled && !useVipps}
+  <div class="container mx-auto pt-0 md:pt-6 lg:pt-8 xl:pt-10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row items-center">
+    <p class="py-2 text-center text-sm sm:text-sm md:text-xl lg:text-xl xl:text-2xl text-white font-sans  md:font-thin lg:font-thin xl:font-thin">
+      prefer to pay with vipps?<br/>
+      click <span class="text-c-orange" on:click="enableVipps()">here</span>.
+    </p>
+
+  </div>
+{/if}
 {/if}
 {#if selectedReceiver}
   <Pay data={selectedReceiver}
@@ -97,6 +106,11 @@ export default {
       if (this.paymentsInitialized) {
         return;
       }
+    },
+    enableVipps() {
+      this.set({
+        useVipps: true,
+      })
     }
   },
   computed: {
