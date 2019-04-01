85d3509fb0399c025848928a73e690153b53d1f4
Bård Røtzer <bard@kartoteket.as>
2019-04-01 14:29:47 +0300

    Add a new version of the map

--- src/classes/MapPoints.js
+++ src/classes/MapPoints.js
@@ -1,9 +1,10 @@
 import Axios from 'axios';
-import L from 'leaflet';
+// import L from 'leaflet';
 import '@/vendor/Leaflet.Geodesic';
 
 export default class Mappoints {
   constructor (map) {
+    console.log(L);
     this.map = map;
     this.data = null;
     this.route = [];
@@ -11,12 +12,13 @@ export default class Mappoints {
 
   createFromTo() {
     const route = [];
-    for (let i = 0; i < this.data.length - 1; i += 1) {
+    for (let i = 0; i < this.data.length; i += 1) {
       const d1 = this.data[i];
-      const from = new L.LatLng(d1.lat, d1.lon);
-      const d2 = this.data[i + 1];
-      const to = new L.LatLng(d2.lat, d2.lon);
-      route.push([from, to])
+      const from = L.latLng(d1.lat, d1.lon);
+      route.push(from);
+      // const d2 = this.data[i + 1];
+      // const to = L.latLng(d2.lat, d2.lon);
+      // route.push([from, to])
     }
     return route;
   }
@@ -24,10 +26,10 @@ export default class Mappoints {
   drawMarkers() {
     const waypoint = L.divIcon({
       className: 'rounded-full waypoint__icon',
-      iconSize: L.point(8, 8)});
+      iconSize: L.point(12, 12)});
     for (let i = 0; i < this.data.length; i += 1) {
       const d = this.data[i];
-      const markerLocation = new L.LatLng(d.lat, d.lon);
+      const markerLocation = L.latLng(d.lat, d.lon);
       const marker = new L.Marker(markerLocation, {icon: waypoint});
       this.map.addLayer(marker);
 
@@ -39,15 +41,21 @@ export default class Mappoints {
 
   drawCurve() {
     const points = this.createFromTo();
-    const geodesi = L.geodesic(points, {
-      weight: 1,
-      opacity: 1,
-      color: '#666',
-      steps: 10,
+    console.log(points);
+    const lines = L.polyline(points, {
+      color: '#333',
+      dashArray: 4,
+      weight: 3,
     }).addTo(this.map);
+    // const geodesi = L.geodesic(points, {
+    //   weight: 1,
+    //   opacity: 1,
+    //   color: '#666',
+    //   steps: 10,
+    // }).addTo(this.map);
 
-    const b = geodesi.getBounds();
-    this.map.fitBounds(b);
+    // const b = geodesi.getBounds();
+    // this.map.fitBounds(b);
   }
 
   show() {
--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,10 +1,11 @@
-<div id="map" on:focus="focusme()" on:blur="blurme()"></div>
+<div id="windy" on:focus="focusme()" on:blur="blurme()"></div>
 
 <script>
-  import L from 'leaflet';
+  // import L from 'leaflet';
   import '@/css/custom.css';
   import MapPoints from '@/classes/MapPoints';
 
+  const L = window.L;
   let map;
 
 	export default {
@@ -20,29 +21,75 @@
       }
     },
 		oncreate() {
-      console.log('created', L);
-      const center = new L.latLng(20.2750, -27.7590);
-      const oceantiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}';
-      const watercolor = 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}';
-			map = L.map('map', {
-        center,
-					zoom: 3
-			});
-      map.scrollWheelZoom.disable();
-      const tilemap = L.tileLayer(oceantiles, {
-        // attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        subdomains: 'abcd',
-        minZoom: 1,
-        maxZoom: 16,
-        ext: 'jpg'
-      }).addTo(map);
 
-      map.on('click', function (a, b, c) {
-        console.log(a,b,c);
-      });
+      const options = {
+        // Required: API key
+        key: 'DYcYlzypFJaDiPqpZC7hZyquXioC1zOl',
+        // Put additional console output
+        verbose: false,
+        // Optional: Initial state of the map
+        lat: 37.11923,
+        lon: -8.527708,
+        zoom: 5,
+        hourFormat: '24h',
+        englishLabels: true,
+      }
 
-      const mapPoints = new MapPoints(map);
-      mapPoints.show();
+    // console.log(windyAPI.getAllowed());
+    // Initialize Windy API
+    windyInit( options, windyAPI => {
+        // windyAPI is ready, and contain 'map', 'store',
+        // 'picker' and other usefull stuff
+
+        const { map, store, picker, utils } = windyAPI
+
+        // picker.on('pickerOpened', latLon => {
+        //     // picker has been opened at latLon coords
+        //     const { lat, lon, values } = picker.getParams();
+        //     // console.log(p);
+        //     // -> 50.4, 14.3, 'wind', [ U,V, ]
+        //     let windObject = utils.wind2obj( values )
+        //     console.log( windObject )
+        // })
+
+        // const cp = picker.open({ lat: options.lat, lon: options.lon });
+
+        // console.log(cp);
+        // .map is instance of Leaflet map
+
+        L.popup()
+            .setLatLng([options.lat, options.lon])
+            .setContent("Current Position")
+            .openOn( map );
+
+        const mapPoints = new MapPoints(map);
+        mapPoints.show();
+
+    })
+
+
+      // console.log('created', L);
+      // const center = new L.latLng(20.2750, -27.7590);
+      // const oceantiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}';
+      // const watercolor = 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}';
+			// map = L.map('map', {
+      //   center,
+			// 		zoom: 3
+      // });
+
+      // map.scrollWheelZoom.disable();
+      // const tilemap = L.tileLayer(oceantiles, {
+      //   // attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      //   subdomains: 'abcd',
+      //   minZoom: 1,
+      //   maxZoom: 16,
+      //   ext: 'jpg'
+      // }).addTo(map);
+
+      // map.on('click', function (a, b, c) {
+      //   console.log(a,b,c);
+      // });
+
 
 		}
 	}
@@ -51,6 +98,7 @@
 	#map {
 		height: 500px;
 		width: 100%
-	}
+  }
+
 
 </style>
