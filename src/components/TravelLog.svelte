b1f25ebfebdcaee9e749a56684eb23f12308b2cf
Bård Røtzer <bard@kartoteket.as>
2019-04-11 12:48:46 +0300

    new travellog added

--- src/components/TravelLog.svelte
+++ src/components/TravelLog.svelte
@@ -1,9 +1,47 @@
-<div>
-  this is the travel log
+<div class="max-w-sm mx-auto pt-4">
+
+  {#each log as logItem}
+    <div class="py-3 logItem border-t">
+      <span class="text-lg font-bold">{logItem.title} - </span> <span class="text-c-darkgrey">{logItem.date}</span>
+        {@html logItem.text}
+    </div>
+  {/each}
 </div>
 <script>
+import Axios from 'axios';
+import moment from 'moment';
+import clientUrl from '@/utils/clientUrl';
+import { sortASC } from '@/utils/date';
+
 export default {
+  data() {
+    return {
+      log: [],
+    }
+  },
+  methods: {
+    getParsed(data) {
+      data.forEach((a) => {
+        const list = a.text.split('\n');
+        a.text = '<p>' + list.join('</p><p>') + '</p>'
+        a.date = moment(new Date(a.date)).fromNow();
+      })
+      data.sort(sortASC);
+      return data;
+    }
+
+
+  },
   oncreate() {
+
+    Axios.get(clientUrl('assets', 'travellog.json'))
+      .then((r) => {
+        console.log(r);
+        const text = this.getParsed(r.data)
+        this.set({
+          log: text,
+        });
+      })
     console.log('created travellog');
   }
 }
