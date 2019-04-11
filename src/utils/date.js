ad778fa181ce272c3180c14a93c940e595ec7e50
Bård Røtzer <bard@kartoteket.as>
2019-04-11 12:20:08 +0300

    Added utility data sorter

---
+++ src/utils/date.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+
+const sortASC = (a, b) => {
+  const isABefore =  moment(a.date).isBefore(b.date);
+  if (isABefore) {
+    return 1
+  } else {
+    return -1;
+  }
+}
+
+export {
+  sortASC,
+}
--- src/utils/pointData.js
+++ src/utils/pointData.js
@@ -1,19 +1,12 @@
 import moment from 'moment';
+import { sortASC } from '@/utils/date';
 
 const parseNews = (data) => {
   if (data && data.length) {
     const newsList = data.filter((d) => {
       return d.comment || d.albums;
     });
-    newsList.sort((a, b) => {
-      console.log(a.date,b.date);
-      const isABefore =  moment(a.date).isBefore(b.date);
-      if (isABefore) {
-        return 1
-      } else {
-        return -1;
-      }
-    });
+    newsList.sort(sortASC);
 
     return newsList;
   }
